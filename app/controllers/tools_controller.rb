bd21946bb4698c4061224ccf201733f0119ecd6c
Hartog C. de Mik <hartog.de.mik@simplic.it>
2010-08-26 14:12:51 +0000

    Gave passphrase generator it's own place, preparing for WSDL analyzer

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,65 +1,31 @@
 class ToolsController < ApplicationController
 
-  @@words      = []
-  CONJUNCTIONS = %w(and or but you me we him + the -)
-  FUZZ         = %w(! @ # $ % ^ & * - + ~ ` , . &lt; > / ?);
-
   def index
   end
 
   def pwgen
+    @@generator ||= PassphraseGenerator.new
     @phrases = []
 
     10.times do 
-      @phrases << get_passphrase
+      @phrases << @@generator.get_passphrase
     end
   end
 
-  def get_passphrase
-    words = get_words()
-    
-    word1 = words[rand(words.length)]
-    word2 = words[rand(words.length)]
-    con = CONJUNCTIONS[rand(CONJUNCTIONS.length)]
-
-    return [ 
-      [ strong_word(word1), con, strong_word(word2) ].join(" "),
-      [ word1, con, word2 ].join(" "),
-    ]
+  def wsdl
   end
 
-  def get_words
-    if @@words == []
-      @@words = IO.readlines("/usr/share/dict/words").collect { |i|
-        i.downcase.gsub("\n", "")
-      }.select { |i|
-        i.length <= 7
-      }
+  def wsdl_analyze
+    contents = nil
+    if params[:url]
+      # load the contents from uri
+    elsif params[:file
+      # load the contents from file
+    else
+      # err
+      raise "No Wisdel, no glory!"
     end
 
-    return @@words;
+    hash = HashWithIndifferentAccess.from_xml(contents)
   end
-
-  def strong_word(word)
-    letters = word.split(//)
-
-    fuzz_pos = rand(letters.length)
-
-    begin
-      letters[fuzz_pos] << FUZZ[rand(FUZZ.length)]
-
-      if letters[fuzz_pos + 1]
-        letters[fuzz_pos+1].upcase!
-      else
-        letters[fuzz_pos][0].upcase!
-      end
-    rescue
-      # fix issues with array length and fixnums by ignoring them
-    end
-    
-
-    return letters.flatten.join("");
-  end
-
-  private :get_passphrase, :strong_word
 end
