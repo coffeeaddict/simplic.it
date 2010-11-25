24e2c2ac158a3f920149ed7e1538e67fd222b92b
Jenny de Mik <jenny@red.(none)>
2010-11-25 21:35:04 +0100

    Getting the Page to work with the couch. Relax, I know what I am doing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  require 'lipsum'
+  
   before_filter :set_menu
 
   def set_menu
@@ -6,9 +8,18 @@ class HomeController < ApplicationController
   end
 
   def index
+    @page = Page.find_by_path "/home/index"
   end
   
   def freelance
+    l = []; (rand(5)+1).times { l << Lipsum.paragraph }
+    @page = Page.new(
+      :title => "Lorem ipsum dolor sit random",
+      :contents => l.join("<br /><br />"),
+      :info => "<h3>More info</h3><p>" + Lipsum.paragraph +
+               "</p><span class='small alt'>do it. hit F5</span>" 
+    )
+    
     render :action => :index
   end
   
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,12 @@
 class Page < CouchRest::Model::Base
-  use_database CouchRest.database!('http://127.0.0.1:5984/pages') 
+  use_database CouchRest.database!('http://192.168.2.4:5984/pages') 
 
   property :title, String
   property :contents, String
-  property :created_at, Time
-  property :updated_at, Time
+  property :path, String
+  property :info, String
+
+  timestamps!
+  
+  view_by :path
 end
