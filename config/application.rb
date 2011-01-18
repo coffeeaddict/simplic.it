5113242b3902594411cf73b68ba34ac84fc485f3
Hartog de Mik <hartog.de.mik@simplic.it>
2011-01-18 21:44:49 +0100

    Load stuff from lib and specify an adapter for dev

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 module Webapp
   class Application < Rails::Application
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{Rails.root}/lib)
     
     config.time_zone = 'UTC'
     config.i18n.default_locale = :en
